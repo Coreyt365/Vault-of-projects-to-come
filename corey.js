c460a8b63b82fd67c299bd0b6aaffaceab91d340
Coreyt365 <40604745+Coreyt365@users.noreply.github.com>
2018-06-27 10:23:31 -0500

    Add files via upload

---
+++ corey.js
@@ -0,0 +1,15 @@
+  function consolePrint(){
+   console.log("hello Corey");
+ }
+  function alertPrint(){
+   alert("hello world-bye world");
+ }
+  function documentPrint(){
+   document.write("hello jon")
+ }
+  function magicPrint(){ 
+    document.write("the magics in the mystery");
+ }
+function bewarePrint(){
+    alert("so you just don't listen");
+}   
